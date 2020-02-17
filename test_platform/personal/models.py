56d028780e88950a88720ae33c3bcd5bfe746e75
anier <498494576@qq.com>
2020-02-18 03:42:44 +0800

    add database config

--- test_platform/personal/models.py
+++ test_platform/personal/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    name=models.CharField(max_length=50,null=False)
+    describe=models.TextField(default="")
+    status=models.BooleanField(default=1)
+    create_time=models.DateTimeField(auto_now_add=True)
