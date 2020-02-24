fc85150b109d7a7016b0645ae8065086cf38e4f5
anier <498494576@qq.com>
2020-02-24 18:48:03 +0800

    add 编辑

--- test_platform/testTask_app/views.py
+++ test_platform/testTask_app/views.py
@@ -1,4 +1,4 @@
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+import json
 from django.shortcuts import render
 from project_app.models import Project
 from model_app.models import Module
@@ -34,35 +34,86 @@ def save_task(request):
         return JsonResponse({"status":10101,"message":"请求方法错误"})
 
 def get_cases_tree(request):
-    projects = Project.objects.all()
-    data_list = []
-    for project in projects:
-        project_dict = {
-            "name":project.name,
-            "isParent": True
-        }
-        modules = Module.objects.filter(project_id=project.id)
-        module_list = []
-        for module in modules:
-            module_dict={
-                "name":module.name,
+    if request.method == "GET":
+        projects = Project.objects.all()
+        data_list = []
+        for project in projects:
+            project_dict = {
+                "name": project.name,
                 "isParent": True
             }
-            cases = TestCase.objects.filter(module_id = module.id)
-            case_list=[]
-            for case in cases:
-                case_dict = {
-                    "name":case.name,
-                    "isParent": False,
-                     "id":case.id
+            modules = Module.objects.filter(project_id=project.id)
+            module_list = []
+            for module in modules:
+                module_dict = {
+                    "name": module.name,
+                    "isParent": True
                 }
-                case_list.append(case_dict)
-            module_dict["children"] = case_list
-            module_list.append(module_dict)
-        project_dict["children"] = module_list
-        data_list.append(project_dict)
-    return  JsonResponse({"status":10200,"message":"success","data":data_list})
+                cases = TestCase.objects.filter(module_id=module.id)
+                case_list = []
+                for case in cases:
+                    case_dict = {
+                        "name": case.name,
+                        "isParent": False,
+                        "id": case.id
+                    }
+                    case_list.append(case_dict)
+                module_dict["children"] = case_list
+                module_list.append(module_dict)
+            project_dict["children"] = module_list
+            data_list.append(project_dict)
+        return JsonResponse({"status": 10200, "message": "success", "data": data_list})
+    if request.method=="POST":
+        tid=request.POST.get("tid","")
+        print("tid: " ,tid)
+        if tid == "":
+            return JsonResponse({"status": 10101, "message": "error!任务id不能为空"})
 
+
+        task = TestTask.objects.get(id=tid)
+        caselist=json.loads(task.cases)
+        task_data = {
+            "name":task.name,
+            "desc":task.describe
+        }
+        projects = Project.objects.all()
+        data_list = []
+        for project in projects:
+            project_dict = {
+                "name": project.name,
+                "isParent": True
+            }
+            modules = Module.objects.filter(project_id=project.id)
+            module_list = []
+            for module in modules:
+                module_dict = {
+                    "name": module.name,
+                    "isParent": True
+                }
+                cases = TestCase.objects.filter(module_id=module.id)
+                case_list = []
+                for case in cases:
+                    if case.id in caselist:
+                        case_dict = {
+                            "name": case.name,
+                            "isParent": False,
+                            "id": case.id,
+                            "checked":True
+                        }
+                    else:
+                        case_dict = {
+                            "name": case.name,
+                            "isParent": False,
+                            "id": case.id,
+                            "checked": False
+                        }
+                    case_list.append(case_dict)
+                module_dict["children"] = case_list
+                module_list.append(module_dict)
+            project_dict["children"] = module_list
+            data_list.append(project_dict)
+        task_data["cases"] = data_list
+        return JsonResponse({"status": 10200, "message": "success", "data": task_data})
 
 
 
